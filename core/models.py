d81b9ca16e33b2dd48da61bc232039cf76647f01
Gabriel Navas <gabnanavas.dev@gmail.com>
2023-02-21 20:51:16 -0300

    change user model

--- core/models.py
+++ core/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from django.contrib.auth.base_user import AbstractBaseUser
 
-# Create your models here.
+class User(AbstractBaseUser):
+    email = models.EmailField(
+        max_length=255,
+        unique=True,
+    )
+
+    USERNAME_FIELD = 'email'
+    
+    class Meta:
+        db_table = 'user'
+    
+    def __str__(self) -> str:
+        return self.email
+
+        
+
+class Profile(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    bio = models.TextField(blank=True)
+    profileimg = models.ImageField(upload_to='profile_images', default='blank-profile-picture.png')
+    location = models.CharField(max_length=100)
+
+    class Meta:
+        db_table = 'profile'
+
+    def __str__(self) -> str:
+        return self.user.get_username()
+
