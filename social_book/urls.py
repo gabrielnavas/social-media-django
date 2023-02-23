61f5c59706d232daf35602dbbab36ee861adb212
Gabriel Navas <gabnanavas.dev@gmail.com>
2023-02-23 20:14:52 -0300

    refactor feed

--- social_book/urls.py
+++ social_book/urls.py
@@ -5,8 +5,8 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('core.urls')),
     path('', include('users.urls')),
+    path('', include('feed.urls')),
 ]
 
 # config from upload image
